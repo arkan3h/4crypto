b9163cbd7791eff9d3569f7f9e0ba7792e140e9b
Muhammad Syihab <syihab.demak@gmail.com>
2024-04-29 20:47:59 +0700

    Finishing Register Data & Adding Firebase

---
+++ app/src/main/java/com/arkan/a4crypto/data/repository/UserRepository.kt
@@ -0,0 +1,82 @@
+package com.arkan.a4crypto.data.repository
+
+import com.arkan.a4crypto.data.datasource.auth.AuthDataSource
+import com.arkan.a4crypto.data.model.User
+import com.arkan.aresto.utils.ResultWrapper
+import com.arkan.aresto.utils.proceedFlow
+import kotlinx.coroutines.flow.Flow
+import java.lang.Exception
+
+interface UserRepository {
+    @Throws(exceptionClasses = [Exception::class])
+    fun doLogin(
+        email: String,
+        password: String,
+    ): Flow<ResultWrapper<Boolean>>
+
+    @Throws(exceptionClasses = [Exception::class])
+    fun doRegister(
+        email: String,
+        fullName: String,
+        password: String,
+    ): Flow<ResultWrapper<Boolean>>
+
+    fun updateProfile(fullName: String? = null): Flow<ResultWrapper<Boolean>>
+
+    fun updatePassword(newPassword: String): Flow<ResultWrapper<Boolean>>
+
+    fun updateEmail(newEmail: String): Flow<ResultWrapper<Boolean>>
+
+    fun requestChangePasswordByEmail(): Boolean
+
+    fun doLogout(): Boolean
+
+    fun isLoggedIn(): Boolean
+
+    fun getCurrentUser(): User?
+}
+
+class UserRepositoryImpl(private val dataSource: AuthDataSource) : UserRepository {
+    override fun doLogin(
+        email: String,
+        password: String,
+    ): Flow<ResultWrapper<Boolean>> {
+        return proceedFlow { dataSource.doLogin(email, password) }
+    }
+
+    override fun doRegister(
+        email: String,
+        fullName: String,
+        password: String,
+    ): Flow<ResultWrapper<Boolean>> {
+        return proceedFlow { dataSource.doRegister(email, fullName, password) }
+    }
+
+    override fun updateProfile(fullName: String?): Flow<ResultWrapper<Boolean>> {
+        return proceedFlow { dataSource.updateProfile(fullName = fullName) }
+    }
+
+    override fun updatePassword(newPassword: String): Flow<ResultWrapper<Boolean>> {
+        return proceedFlow { dataSource.updatePassword(newPassword) }
+    }
+
+    override fun updateEmail(newEmail: String): Flow<ResultWrapper<Boolean>> {
+        return proceedFlow { dataSource.updateEmail(newEmail) }
+    }
+
+    override fun requestChangePasswordByEmail(): Boolean {
+        return dataSource.requestChangePasswordByEmail()
+    }
+
+    override fun doLogout(): Boolean {
+        return dataSource.doLogout()
+    }
+
+    override fun isLoggedIn(): Boolean {
+        return dataSource.isLoggedIn()
+    }
+
+    override fun getCurrentUser(): User? {
+        return dataSource.getCurrentUser()
+    }
+}
