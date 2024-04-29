d2833e5a49bef815a727fa6bb729373e5627e0a9
Arkan Mahardika <arkan.mahardika.1@gmail.com>
2024-04-29 20:49:27 +0700

    Init detail activity

---
+++ app/src/main/java/com/arkan/a4crypto/presentation/detail/DetailActivity.kt
@@ -0,0 +1,26 @@
+package com.arkan.a4crypto.presentation.detail
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.arkan.a4crypto.R
+import com.arkan.a4crypto.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    private val binding : ActivityDetailBinding by lazy {
+        ActivityDetailBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
