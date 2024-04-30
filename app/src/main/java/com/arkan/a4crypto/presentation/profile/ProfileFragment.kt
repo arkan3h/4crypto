7e7330d04ef95b22a094e8b7a386466a9a7456b4
rian syah <rsyah1641@gmail.com>
2024-04-30 21:33:42 +0700

    set profile part 1

--- app/src/main/java/com/arkan/a4crypto/presentation/profile/ProfileFragment.kt
+++ app/src/main/java/com/arkan/a4crypto/presentation/profile/ProfileFragment.kt
@@ -5,9 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.arkan.a4crypto.R
+import com.arkan.a4crypto.data.datasource.auth.AuthDataSource
+import com.arkan.a4crypto.data.datasource.auth.FirebaseAuthDataSource
+import com.arkan.a4crypto.data.repository.UserRepository
+import com.arkan.a4crypto.data.repository.UserRepositoryImpl
+import com.arkan.a4crypto.data.source.firebase.FirebaseService
+import com.arkan.a4crypto.data.source.firebase.FirebaseServiceImpl
+import com.arkan.a4crypto.databinding.FragmentProfileBinding
+import com.arkan.aresto.utils.GenericViewModelFactory
 
 class ProfileFragment : Fragment() {
+    private lateinit var binding: FragmentProfileBinding
+
+    private val viewModel: ProfileViewModel by viewModels {
+        val user: FirebaseService = FirebaseServiceImpl()
+        val userDataSource: AuthDataSource = FirebaseAuthDataSource(user)
+        val userRepo: UserRepository = UserRepositoryImpl(userDataSource)
+        GenericViewModelFactory.create(ProfileViewModel(userRepo))
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
---
+++ app/src/main/java/com/arkan/a4crypto/presentation/profile/ProfileViewModel.kt
@@ -0,0 +1,31 @@
+package com.arkan.a4crypto.presentation.profile
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.arkan.a4crypto.data.repository.UserRepository
+import kotlinx.coroutines.Dispatchers
+
+class ProfileViewModel(private val repo: UserRepository) : ViewModel() {
+
+    val isEditMode = MutableLiveData(false)
+
+    fun changeEditMode() {
+        val currentValue = isEditMode.value ?: false
+        isEditMode.postValue(!currentValue)
+    }
+
+    fun changeProfile(fullName: String) = repo.updateProfile(fullName).asLiveData(Dispatchers.IO)
+
+    fun changePassword() {
+        repo.requestChangePasswordByEmail()
+    }
+    fun getCurrentUser() = repo.getCurrentUser()
+
+
+    fun isUserLoggedIn() = repo.isLoggedIn()
+
+    fun doLogout() {
+        repo.doLogout()
+    }
+}
