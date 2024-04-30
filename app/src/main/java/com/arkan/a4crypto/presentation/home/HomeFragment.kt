243d4a65fe12d14969768102dc226f9c51ed4700
Arkan Mahardika <arkan.mahardika.1@gmail.com>
2024-04-30 20:25:12 +0700

    Rebase master

--- app/src/main/java/com/arkan/a4crypto/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/arkan/a4crypto/presentation/home/HomeFragment.kt
@@ -1,28 +1,14 @@
 package com.arkan.a4crypto.presentation.home
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
-import com.arkan.a4crypto.R
-import com.arkan.a4crypto.data.model.Coin
 import com.arkan.a4crypto.databinding.FragmentHomeBinding
-import com.arkan.a4crypto.presentation.detail.DetailActivity
-import com.arkan.a4crypto.presentation.home.adapter.CoinAdapter
-import com.arkan.a4crypto.presentation.home.adapter.OnItemCLickedListener
-import com.arkan.aresto.utils.proceedWhen
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
-    private var coinAdapter: CoinAdapter? = null
-    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
-    private val homeViewModel: HomeViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,84 +19,10 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
-    private fun showUserData() {
-        homeViewModel.getCurrentUser()?.let { user ->
-            binding.layoutBanner.tvGreetings.text =
-                getString(R.string.text_greetings, user.fullName)
-        }
-    }
-
     override fun onViewCreated(
         view: View,
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
-        getCoinData()
-        refreshLayout()
-        showUserData()
-    }
-
-    private fun refreshLayout() {
-        swipeRefreshLayout = binding.swipeHome
-        swipeRefreshLayout.setOnRefreshListener {
-            getCoinData()
-            Handler().postDelayed(
-                { swipeRefreshLayout.isRefreshing = false },
-                1000,
-            )
-        }
-    }
-
-    private fun getCoinData() {
-        homeViewModel.getCoinList().observe(viewLifecycleOwner) { it ->
-            it.proceedWhen(
-                doOnLoading = {
-                    binding.layoutState.pbLoading.isVisible = true
-                    binding.layoutState.tvError.isVisible = false
-                    binding.rvListCoin.isVisible = false
-                },
-                doOnError = {
-                    binding.layoutState.pbLoading.isVisible = false
-                    binding.layoutState.tvError.isVisible = true
-                    binding.rvListCoin.isVisible = false
-                },
-                doOnSuccess = {
-                    binding.layoutState.pbLoading.isVisible = false
-                    binding.layoutState.tvError.isVisible = false
-                    binding.rvListCoin.isVisible = true
-                    it.payload?.let { data ->
-                        bindCoinList(data)
-                    }
-                },
-                doOnEmpty = {
-                    binding.layoutState.pbLoading.isVisible = false
-                    binding.layoutState.tvError.isVisible = true
-                    binding.rvListCoin.isVisible = false
-                },
-            )
-        }
-    }
-
-    private fun bindCoinList(data: List<Coin>) {
-        coinAdapter =
-            CoinAdapter(
-                listener =
-                    object : OnItemCLickedListener<Coin> {
-                        override fun onItemClicked(item: Coin) {
-                            navigateToDetail(item)
-                        }
-                    },
-            )
-        binding.rvListCoin.adapter = this@HomeFragment.coinAdapter
-        coinAdapter?.submitData(data)
-    }
-
-    private fun navigateToDetail(item: Coin) {
-        startActivity(
-            Intent(activity, DetailActivity::class.java).putExtra(
-                DetailActivity.EXTRAS_ITEM_ACT,
-                item,
-            ),
-        )
     }
 }
--- app/src/main/java/com/arkan/a4crypto/presentation/home/adapter/CoinAdapter.kt
+++ app/src/main/java/com/arkan/a4crypto/presentation/home/adapter/CoinAdapter.kt
@@ -7,12 +7,10 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.arkan.a4crypto.data.model.Coin
-import com.arkan.a4crypto.databinding.ItemHomeListCoinBinding
+import com.arkan.a4crypto.databinding.ItemHomeListCointBinding
 import com.arkan.aresto.utils.toDollarFormat
 
-class CoinAdapter(
-    private val listener: OnItemCLickedListener<Coin>,
-) : RecyclerView.Adapter<CoinAdapter.CoinViewHolder>() {
+class CoinAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private val asyncDataDiffer =
         AsyncListDiffer(
             this,
@@ -42,41 +40,34 @@ class CoinAdapter(
         viewType: Int,
     ): CoinViewHolder {
         return CoinViewHolder(
-            ItemHomeListCoinBinding.inflate(
+            ItemHomeListCointBinding.inflate(
                 LayoutInflater.from(parent.context),
                 parent,
                 false,
             ),
-            listener,
         )
+    }
+
+    override fun onBindViewHolder(
+        holder: RecyclerView.ViewHolder,
+        position: Int,
+    ) {
+        (holder as ViewHolderBinder<Coin>).bind(asyncDataDiffer.currentList[position])
     }
 
     override fun getItemCount(): Int = asyncDataDiffer.currentList.size
 
-    override fun onBindViewHolder(
-        holder: CoinViewHolder,
-        position: Int,
-    ) {
-        holder.bind(asyncDataDiffer.currentList[position])
-    }
-
-    class CoinViewHolder(
-        private val binding: ItemHomeListCoinBinding,
-        private val listener: OnItemCLickedListener<Coin>,
-    ) :
+    class CoinViewHolder(private val binding: ItemHomeListCointBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(item: Coin) {
             binding.ivImgItemHome.load(item.image)
             binding.tvItemHomeTitle.text = item.name
             binding.tvItemHomeDesk.text = item.desc
             binding.tvItemHomePrice.text = item.price.toDollarFormat()
-            itemView.setOnClickListener {
-                listener.onItemClicked(item)
-            }
         }
     }
 }
 
-interface OnItemCLickedListener<T> {
-    fun onItemClicked(item: T)
+interface ViewHolderBinder<T> {
+    fun bind(item: T)
 }
