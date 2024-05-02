2c9d25bdede456f20795c2dd5a52804f9e1344c1
Arkan Mahardika <arkan.mahardika.1@gmail.com>
2024-05-02 07:52:23 +0700

    Add OnClickListener in list coin

--- app/src/main/java/com/arkan/a4crypto/data/model/Coin.kt
+++ app/src/main/java/com/arkan/a4crypto/data/model/Coin.kt
@@ -1,9 +1,13 @@
 package com.arkan.a4crypto.data.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Coin(
     val id: String,
     val name: String,
     val desc: String,
     val image: String,
     val price: Double,
-)
+) : Parcelable
