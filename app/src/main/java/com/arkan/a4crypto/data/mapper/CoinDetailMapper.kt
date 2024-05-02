2b0563c22774c94f1797c0bbd5874caf615079ef
Arkan Mahardika <arkan.mahardika.1@gmail.com>
2024-05-02 07:04:10 +0700

    Add Coin Detail to api service

---
+++ app/src/main/java/com/arkan/a4crypto/data/mapper/CoinDetailMapper.kt
@@ -0,0 +1,19 @@
+package com.arkan.a4crypto.data.mapper
+
+import com.arkan.a4crypto.data.model.CoinDetail
+import com.arkan.a4crypto.data.source.network.model.CoinDetailResponse
+
+fun CoinDetailResponse.toCoinDetail() =
+    CoinDetail(
+        id = this.id.orEmpty(),
+        name = this.name.orEmpty(),
+        symbol = this.symbol.orEmpty(),
+        image = this.image?.large.orEmpty(),
+        desc = this.description?.en.orEmpty(),
+        price = this.market?.price?.usd ?: 0.0,
+    )
+
+fun Collection<CoinDetailResponse>.toCoinDetail() =
+    this.map {
+        it.toCoinDetail()
+    }
