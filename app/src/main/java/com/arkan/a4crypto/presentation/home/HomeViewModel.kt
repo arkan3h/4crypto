5bba0f0a1e47a8d6f727b257af360c2fe82c1a3b
Marcella <marcellaandr22@gmail.com>
2024-05-02 19:10:57 +0700

    Refactor Home Banner

--- app/src/main/java/com/arkan/a4crypto/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/arkan/a4crypto/presentation/home/HomeViewModel.kt
@@ -3,10 +3,14 @@ package com.arkan.a4crypto.presentation.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.arkan.a4crypto.data.repository.CoinRepository
+import com.arkan.a4crypto.data.repository.UserRepository
 import kotlinx.coroutines.Dispatchers
 
 class HomeViewModel(
     private val coinRepository: CoinRepository,
+    private val userRepository: UserRepository,
 ) : ViewModel() {
     fun getCoinList() = coinRepository.getCoinList().asLiveData(Dispatchers.IO)
+
+    fun getCurrentUser() = userRepository.getCurrentUser()
 }
