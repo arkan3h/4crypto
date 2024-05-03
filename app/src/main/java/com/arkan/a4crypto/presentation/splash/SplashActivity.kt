8a1beb25330489460a28d24135a49f9a16063667
Arkan Mahardika <arkan.mahardika.1@gmail.com>
2024-05-03 14:21:01 +0700

    Fix splashscreen background & register layout

--- app/src/main/java/com/arkan/a4crypto/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/arkan/a4crypto/presentation/splash/SplashActivity.kt
@@ -2,8 +2,12 @@ package com.arkan.a4crypto.presentation.splash
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
+import com.arkan.a4crypto.R
 import com.arkan.a4crypto.databinding.ActivitySplashBinding
 import com.arkan.a4crypto.presentation.login.LoginActivity
 import com.arkan.a4crypto.presentation.main.MainActivity
@@ -19,7 +23,13 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.motionLayout)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            insets
+        }
         checkIfUserLogin()
     }
 
